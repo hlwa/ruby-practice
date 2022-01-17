d1a85e5a437f13c427970d2951fdf6f543f3993a
Hailan Wang <hailan6257@gmail.com>
2022-01-17 15:08:08 -0500

    Done

---
+++ w7/fancy-buttons/src/components/TextRepeaterButton.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+  function TextRepeaterButton(){
+    const [repetitions, setRepetitions] = useState(1);
+    const textArray = [];
+    const handleClick = () => setRepetitions(repetitions + 1);
+    for (let i = 0; i < repetitions; i++) {
+      textArray.push(<span key={i}>I like this text</span>);
+    }
+    // ...
+    return (
+      <button onClick={handleClick} className="TextRepeaterButton">
+        {textArray}
+      </button>
+    );
+  }
+
+  export default TextRepeaterButton;
